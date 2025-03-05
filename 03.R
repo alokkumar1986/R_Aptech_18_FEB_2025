406d0f80386d4a091fe740253c72a081e7bccdf9
Alok Kumar Nayak <alokkumar.nayak2009@gmail.com>
2025-03-05 08:47:55 +0530

    '05-03-2025'

---
+++ 03.R
@@ -0,0 +1,26 @@
+str <- "Aptech Learning is a offline training institute based 
+in bhubaneswar. The courses provided here are Python, R, MySQL, 
+Data Science etc"
+
+str
+cat(str)
+len <- nchar(str)
+len
+
+a <- "aptech"
+b <- "learning"
+paste(a, b)
+
+
+str <- "Aptech Learning is a offline training institute based 
+in bhubaneswar. The courses provided here are Python, R, MySQL, 
+Data Science etc"
+c <- grepl("Aptech123", str)
+c
+
+
+
+
+
+
+
